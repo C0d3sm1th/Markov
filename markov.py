f1999c95992d976619968bf99dc0e488fb17bfbb
David Smith <dsmith@governmentbrands.com>
2024-04-14 18:11:21 -0400

    Added plots and q learning

--- markov.py
+++ markov.py
@@ -35,7 +35,7 @@ def get_policy_map(pi, val_max, actions, map_size):
     val_max = val_max.reshape(map_size[0], map_size[1])
     return val_max, policy_map
 
-def plot_policy(val_max, directions, map_size, title):
+def plot_policy(val_max, directions, map_size, title, fontsize="small"):
     """Plot the policy learned."""
     sns.heatmap(
         val_max,
@@ -46,56 +46,191 @@ def plot_policy(val_max, directions, map_size, title):
         linecolor="black",
         xticklabels=[],
         yticklabels=[],
-        annot_kws={"fontsize": "xx-large"},
+        annot_kws={"fontsize": fontsize},
     ).set(title=title)
     img_title = f"Policy_{map_size[0]}x{map_size[1]}.png"
     save_plot_file(title)
 
-gamma=[.8,.9,1.0]
-n_iters=[10,20,50]
-theta=[.1,.01,.001]
-base_env = gym.make('Blackjack-v1', render_mode='rgb_array')
-base_env.reset(seed=44)
-blackjack = BlackjackWrapper(base_env)
-# GridSearch.vi_grid_search(blackjack, gamma, n_iters, theta)
-# GridSearch.pi_grid_search(blackjack, gamma, n_iters, theta)
+def values_heat_map(data, title, size):
+    data = np.around(np.array(data).reshape(size), 2)
+    df = pd.DataFrame(data=data)
+    sns.heatmap(df, annot=True).set_title(title)
+    save_plot_file(title)
 
-gamma = 1.0
-iterations = 50
-theta=0.001
+def run_blackjack(seed=44):
 
-# run VI
-# V, V_track, pi = Planner(blackjack.P).value_iteration()
-V, V_track, pi = Planner(blackjack.P).value_iteration(gamma=gamma, n_iters=iterations, theta=theta)
-max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
-v_iters_plot(max_value_per_iter, "Blackjack Mean Value v Iterations - VI")
+    gamma=[0.0]
+    n_iters=[10,20,30]
+    theta=[.001,.0001]
+    base_env = gym.make('Blackjack-v1', render_mode='rgb_array')
+    base_env.reset(seed=seed)
+    blackjack = BlackjackWrapper(base_env)
+    # GridSearch.vi_grid_search(blackjack, gamma, n_iters, theta)
+    # GridSearch.pi_grid_search(blackjack, gamma, n_iters, theta)
 
-blackjack_actions = {0: "S", 1: "H"}
-blackjack_map_size=(29, 10)
+    gamma = 1.0
+    iterations = 1000
+    theta = .0001
 
-val_max, policy_map = get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
-
-title="Blackjack Policy Map - VI"
-plot_policy(val_max, policy_map, blackjack_map_size, title)
-
-gamma=1.0
-iterations = 20
-theta=0.01
-
-#test policy
-V_pi, V_track_pi, pi_pi = Planner(blackjack.P).policy_iteration(gamma=gamma, n_iters=iterations, theta=theta)
-max_value_per_iter_pi = np.trim_zeros(np.mean(V_track_pi, axis=1), 'b')
-v_iters_plot(max_value_per_iter_pi, "Blackjack Mean Value v Iterations - PI")
-
-val_max, policy_map = get_policy_map(pi_pi, V_pi, blackjack_actions, blackjack_map_size)
-
-title="Blackjack Policy Map - PI"
-plot_policy(val_max, policy_map, blackjack_map_size, title)
+    # run VI
+    V, V_track, pi = Planner(blackjack.P).value_iteration(gamma=gamma, n_iters=iterations, theta=theta)
+    episode_rewards = TestEnv.test_env(env=blackjack, render=False, user_input=False, pi=pi)
+    print(f'Policy mean score VI - Blackjack: {np.mean(episode_rewards)}')
 
 
-# Q-learning
-Q, V, pi, Q_track, pi_track = RL(blackjack).q_learning(gamma=gamma)
+    max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
+    v_iters_plot(max_value_per_iter, "Blackjack Mean Value v Iterations - VI")
 
-#test policy
-test_scores = TestEnv.test_env(env=blackjack, n_iters=iterations, render=False, pi=pi, user_input=False)
-print(f'Test policy mean score Q Learning: {np.mean(test_scores)}')
+    blackjack_actions = {0: "S", 1: "H"}
+    blackjack_map_size=(29, 10)
+
+    val_max, policy_map = get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
+
+    title="Blackjack Policy Map - VI"
+    plot_policy(val_max, policy_map, blackjack_map_size, title)
+
+    gamma = 1.0
+    iterations = 1000
+    theta = .0001
+
+    #run PI
+    V_pi, V_track_pi, pi_pi = Planner(blackjack.P).policy_iteration(gamma=gamma, n_iters=iterations, theta=theta)
+    episode_rewards_pi = TestEnv.test_env(env=blackjack, render=False, user_input=False, pi=pi_pi)
+    print(f'Policy mean score PI - Blackjack: {np.mean(episode_rewards_pi)}')
+
+    max_value_per_iter_pi = np.trim_zeros(np.mean(V_track_pi, axis=1), 'b')
+    v_iters_plot(max_value_per_iter_pi, "Blackjack Mean Value v Iterations - PI")
+
+    val_max, policy_map = get_policy_map(pi_pi, V_pi, blackjack_actions, blackjack_map_size)
+
+    title="Blackjack Policy Map - PI"
+    plot_policy(val_max, policy_map, blackjack_map_size, title)
+
+def run_blackjack_q(seed=44):
+
+    base_env = gym.make('Blackjack-v1', render_mode='rgb_array')
+    base_env.reset(seed=seed)
+    blackjack = BlackjackWrapper(base_env)
+
+    gammas=[.1,.5,.9]
+    epsilon_decays = [0.01,0.1,0.9]
+    iters = [5000, 10000]
+    # GridSearch.q_learning_grid_search(blackjack, gammas, epsilon_decays, iters)    
+
+    gamma = 0.5
+    iterations = 5000
+    epsilon_decay = 0.9
+
+    # Q-learning
+    Q, V, pi, Q_track, pi_track = RL(blackjack).q_learning(gamma=gamma, epsilon_decay_ratio=epsilon_decay, n_episodes=iterations)
+
+    #test policy
+    episode_rewards = TestEnv.test_env(env=blackjack, n_iters=100, pi=pi)
+    print(f'Policy mean score Q Learning - Blackjack: {np.mean(episode_rewards)}')
+
+    # max_value_per_iter_q = np.trim_zeros(np.mean(Q_track, axis=1), 'b')
+    # v_iters_plot(max_value_per_iter_q, "Blackjack Mean Value v Iterations - Q Learning")
+    # v_iters_plot(episode_rewards, "Blackjack Rewards v Episodes - Q Learning")
+
+    blackjack_actions = {0: "S", 1: "H"}
+    blackjack_map_size=(29, 10)
+
+    val_max, policy_map = get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
+
+    title="Blackjack Policy Map - Q Learning"
+    plot_policy(val_max, policy_map, blackjack_map_size, title)
+
+def run_taxi(seed=44):
+
+    # make gym environment 
+    taxi = gym.make('Taxi-v3', render_mode=None)
+    taxi.reset(seed=seed)
+
+    # gamma=[.99]
+    # epsilon_decay = [.9]
+    # iters = [500, 5000, 50000]
+    # GridSearch.q_learning_grid_search(taxi, gamma, epsilon_decay, iters)    
+
+    iterations = 1000
+    gamma = 0.99
+    theta = 0.001
+
+    V, V_track, pi = Planner(taxi.P).value_iteration(n_iters=iterations, gamma=gamma, theta=theta)
+
+    episode_rewards = TestEnv.test_env(env=taxi, render=False, user_input=False, pi=pi)
+    print(f'Policy mean score VI - Taxi: {np.mean(episode_rewards)}')
+
+    max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
+    v_iters_plot(max_value_per_iter, "Taxi Mean Value v Iterations - VI")
+
+    # size=(20, 25)
+    # values_heat_map(V, "Taxi Value Iteration State Values - VI", size)      
+    taxi_actions = {0: "←", 1: "↓", 2: "→", 3: "↑", 4: "P", 5: "D"}
+    taxi_map_size=(20, 25)
+    title="Taxi Policy Map - VI"
+    val_max, policy_map = get_policy_map(pi, V, taxi_actions, taxi_map_size)
+    plot_policy(val_max, policy_map, taxi_map_size, title, "small")
+
+
+    iterations = 5000
+    gamma = 0.9
+    theta = 0.0001
+
+    V_pi, V_track_pi, pi_pi = Planner(taxi.P).policy_iteration(n_iters=iterations, gamma=gamma, theta=theta)
+
+    episode_rewards_pi = TestEnv.test_env(env=taxi, render=False, user_input=False, pi=pi_pi)
+    print(f'Policy mean score PI - Taxi: {np.mean(episode_rewards_pi)}')
+
+
+    max_value_per_iter_pi = np.trim_zeros(np.mean(V_track_pi, axis=1), 'b')
+    v_iters_plot(max_value_per_iter_pi, "Taxi Mean Value v Iterations - PI")    
+
+    title="Taxi Policy Map - PI"
+    val_max, policy_map = get_policy_map(pi_pi, V_pi, taxi_actions, taxi_map_size)
+    plot_policy(val_max, policy_map, taxi_map_size, title, "small")
+
+    # episodes = range(len(episode_rewards))
+    # plt.plot(episodes, episode_rewards, label="Value Iteration")
+    # plt.plot(episodes, episode_rewards_pi, label="Policy Iteration")
+    # plt.xlabel("Episodes")
+    # plt.ylabel("Average Reward")
+    # plt.title("Policy Convergence Comparison (Taxi MDP)")
+    # plt.legend()
+    # plt.show()
+
+    print(episode_rewards)
+
+def run_taxi_q(seed=44):
+
+    taxi = gym.make('Taxi-v3', render_mode=None)
+    taxi.reset(seed=seed)
+
+    gammas=[.9,.99]
+    epsilon_decays = [.01,.1]
+    iters = [5000,10000]
+    # GridSearch.q_learning_grid_search(taxi, gammas, epsilon_decays, iters)    
+
+    iterations = 10000
+    gamma = 0.9
+    epsilon_decay = 0.1
+
+    # Q-learning
+    Q, V_q, pi_q, Q_track, pi_track = RL(taxi).q_learning(gamma=gamma, n_episodes=iterations, epsilon_decay_ratio=epsilon_decay)
+
+    #test policy
+    test_scores = TestEnv.test_env(env=taxi, n_iters=iterations, render=False, pi=pi_q, user_input=False)
+    print(f'Policy mean score Q Learning - Taxi: {np.mean(test_scores)}')
+    # max_value_per_iter_q = np.trim_zeros(np.mean(Q_track, axis=1), 'b')
+
+    taxi_actions = {0: "←", 1: "↓", 2: "→", 3: "↑", 4: "P", 5: "D"}
+    taxi_map_size=(20, 25)
+    val_max, policy_map = get_policy_map(pi_q, V_q, taxi_actions, taxi_map_size)
+
+    title="Taxi Policy Map - Q Learning"
+    plot_policy(val_max, policy_map, taxi_map_size, title)
+
+
+run_blackjack(44)
+run_blackjack_q(44)
+run_taxi(44)
+run_taxi_q(44)
